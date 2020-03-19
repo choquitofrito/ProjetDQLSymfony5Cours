b935b7fef5fa637031265664d14184aeb42c373f
choquitofrito <zyriab@gmail.com>
2020-03-19 15:52:43 +0100

    livre exemplaire

--- src/Controller/ExemplesDqlController.php
+++ src/Controller/ExemplesDqlController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class ExemplesDqlController extends AbstractController
 {
     /**
-     * @Route("/exemples/dql/exemple/select/array/arrays", name="exemples_dql")
+     * @Route("/exemples/dql/exemple/select/array/arrays")
      */
     public function exempleSelectArrayArrays()
     {
@@ -18,8 +18,30 @@ class ExemplesDqlController extends AbstractController
         return $this->render('exemples_dql/exemple_select_array_arrays.html.twig');
     }
 
-    
+    // exemple d'obtention d'un array d'objets
 
+    /**
+     * @Route("/exemples/dql/exemple/obtenir/exemplaires")
+     */
+    public function exempleObtenirExemplaires (){
+        $em = $this->getDoctrine()->getManager();
+        $query = $em->createQuery ("SELECT exemplaire FROM App\Entity\Exemplaire exemplaire");
+        dd ($query->getResult());
+    }
+
+    // join
+
+    /**
+     * @Route ("/exemples/dql/exemple/obtenir/livre/avec/exemplaires")
+     */
+    public function exempleObtenirLivreAvecExemplaires (){
+        $em = $this->getDoctrine()->getManager();
+        $query = $em->createQuery ("SELECT livre FROM App\Entity\Livre livre JOIN livre.exemplaires");
+        dd ($query->getResult());
+        
+        
+
+    }
 
 
 }
