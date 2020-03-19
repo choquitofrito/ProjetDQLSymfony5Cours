6c9e384dcc7135034cd298fe8b92d87f1b85c132
choquitofrito <zyriab@gmail.com>
2020-03-19 16:47:19 +0100

    regular et fetch join

--- src/Controller/ExemplesDqlController.php
+++ src/Controller/ExemplesDqlController.php
@@ -13,8 +13,8 @@ class ExemplesDqlController extends AbstractController
     public function exempleSelectArrayArrays()
     {
         $em = $this->getDoctrine()->getManager();
-        $query = $em->createQuery ("SELECT livre.titre, livre.prix FROM App\Entity\Livre livre");
-        dd ($query->getResult());
+        $query = $em->createQuery("SELECT livre.titre, livre.prix FROM App\Entity\Livre livre");
+        dd($query->getResult());
         return $this->render('exemples_dql/exemple_select_array_arrays.html.twig');
     }
 
@@ -23,22 +23,38 @@ class ExemplesDqlController extends AbstractController
     /**
      * @Route("/exemples/dql/exemple/obtenir/exemplaires")
      */
-    public function exempleObtenirExemplaires (){
+    public function exempleObtenirExemplaires()
+    {
         $em = $this->getDoctrine()->getManager();
-        $query = $em->createQuery ("SELECT exemplaire FROM App\Entity\Exemplaire exemplaire");
-        dd ($query->getResult());
+        $query = $em->createQuery("SELECT exemplaire FROM App\Entity\Exemplaire exemplaire");
+        dd($query->getResult());
     }
 
-    // join
+    // Regular Join vs Fetch Join
 
     /**
      * @Route ("/exemples/dql/exemple/obtenir/livre/avec/exemplaires")
      */
-    public function exempleObtenirLivreAvecExemplaires (){
+    public function exempleObtenirLivreAvecExemplaires()
+    {
         $em = $this->getDoctrine()->getManager();
-        $query = $em->createQuery ("SELECT livre FROM App\Entity\Livre livre JOIN livre.exemplaires exemplaires");
-        dd ($query->getResult());
+        $queryRegular = $em->createQuery("SELECT livre FROM App\Entity\Livre livre JOIN livre.exemplaires exemplaires");
+        $livres = $queryRegular->getResult();
+        // on obtient tous les exemplaires du prémier livre
+        $exemplaires = $livres[0]->getExemplaires();
+        dump ($exemplaires); // la collection apparait vide si on ne rajoute pas "exemplaires" dans la requête
+        dump ($exemplaires[0]); // quand on accède diréctement à l'exemplaire, il est là
+        dump ($exemplaires[1]); // pareil pour l'autre exemplaire
+
+        $queryFetch = $em->createQuery("SELECT livre, exemplaires FROM App\Entity\Livre livre JOIN livre.exemplaires exemplaires");
+        $livres = $queryFetch->getResult();
+        // on obtient tous les exemplaires du prémier livre
+        $exemplaires = $livres[0]->getExemplaires();
+        dump ($exemplaires); // la collection apparait remplie car on a rajouté "exemplaires" dans la requête
+        dump ($exemplaires[0]); // quand on accède diréctement à l'exemplaire, il est aussi là
+        dump ($exemplaires[1]); // pareil pour l'autre exemplaire
+
+        die();
+        
     }
-
-
 }
