acfa3a3f606503804ee3b58f76514634eee75dcc
choquitofrito <zyriab@gmail.com>
2020-03-19 15:14:44 +0100

    ok

---
+++ src/Controller/ExemplesDqlController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ExemplesDqlController extends AbstractController
+{
+    /**
+     * @Route("/exemples/dql/exemple/select/array/arrays", name="exemples_dql")
+     */
+    public function exempleSelectArrayArrays()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $query = $em->createQuery ("SELECT livre.titre,livre.prix FROM App\Entity\Livre livre");
+        
+        return $this->render('exemples_dql/exemple_select_array_arrays.html.twig');
+    }
+}
